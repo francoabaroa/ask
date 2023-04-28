6f2895fe2bb007b9ca09ea766657fe9ca564c74f
Franco Abaroa <franco9395@gmail.com>
2023-04-28 15:37:16 -0400

    Change pdf to be about golden compass and fix bug in max section length

--- backend/app/controllers/api/v1/questions_controller.rb
+++ backend/app/controllers/api/v1/questions_controller.rb
@@ -43,11 +43,11 @@ module Api
             cache_answer(user_question, answer)
           else
             # Read pages file
-            pages_csv = File.join(Rails.root, 'resources', 'pdf-sample.pdf.pages.csv')
+            pages_csv = File.join(Rails.root, 'resources', 'goldencompass.pdf.pages.csv')
             df = CSV.read(pages_csv, headers: true)
 
             # Load embeddings file
-            embeddings_csv = File.join(Rails.root, 'resources', 'pdf-sample.pdf.embeddings.csv')
+            embeddings_csv = File.join(Rails.root, 'resources', 'goldencompass.pdf.embeddings.csv')
             document_embeddings = load_embeddings(embeddings_csv)
 
             # Answer question with context and get answer and context for saving
@@ -110,30 +110,29 @@ module Api
               chosen_sections_len += tokens + separator_len
               if chosen_sections_len > MAX_SECTION_LEN
                 space_left = MAX_SECTION_LEN - chosen_sections_len - SEPARATOR.length
-                chosen_sections.append(SEPARATOR + content[0, space_left])
+                chosen_sections.append(SEPARATOR + content[0...space_left])
                 chosen_sections_indexes.append(section_index.to_s)
                 break
               end
-
               chosen_sections.append(SEPARATOR + content)
               chosen_sections_indexes.append(section_index.to_s)
             end
           end
 
-          header = """Sahil Lavingia is the founder and CEO of Gumroad, and the author of the book The Minimalist Entrepreneur (also known as TME). These are questions and answers by him. Please keep your answers to three sentences maximum, and speak in complete sentences. Stop speaking once your point is made.\n\nContext that may be useful, pulled from The Minimalist Entrepreneur:\n"""
+          header = """Philip Pullman is the author of the book The Golden Compass (also known as His Dark Materials). Please keep your answers to three sentences maximum, and speak in complete sentences. Stop speaking once your point is made.\n\nContext that may be useful, pulled from The Golden Compass:\n"""
+          question_1 = "\n\n\nQ: Who is Lyra's daemon?\n\nA: Lyra's daemon is Pantalaimon, also known as Pan. He is a shape-shifting creature that takes the form of various animals, but he typically prefers the form of an ermine. He is a constant companion to Lyra."
+          question_2 = "\n\n\nQ: Q: What is Tokay?\n\nA: Tokay is a type of sweet dessert wine. It is made from grapes grown in the Tokaj region of Hungary. It is known for its golden color and rich flavor."
+          question_3 = "\n\n\nQ: Who tried to poison Lord Asriel?\n\nA: The Master of Jordan College, Dr. Carne, tried to poison Lord Asriel by putting poison in his wine."
+          question_4 = "\n\n\nQ: Where does Tony live?\n\nA: Tony lives in Clarice Walk."
+          question_5 = "\n\n\nQ: What does the Retiring Room look like?\n\nA: The Retiring Room is a large room with an oval table of polished rosewood, various decanters and glasses, a silver smoking stand with a rack of pipes, a chafing dish, and a basket of poppy heads. The walls are adorned with portraits of old Scholars."
+          question_6 = "\n\n\nQ: Who are The Gobblers?\n\nA: The Gobblers are a mysterious group of enchanters who are said to kidnap children and take them away to unknown places."
+          question_7 = "\n\n\nQ: What years was Simon Le Clerc master? \n\nA: 1765-1789."
+          question_8 = "\n\n\nQ: Who is Ma Costa's daemon?\n\nA: Ma Costa's daemon is a hawk."
+          question_9 = "\n\n\nQ: Who is Hugh Lovat?\n\nA: A kitchen boy from St. Michael's."
+          question_10 = "\n\n\nQ: Who is Colonel Carborn?\n\nA: Colonel Carborn is an elderly gentleman with a red tie who made the first balloon flight over the North Pole."
+          question_11 = "\n\n\nQ: Who is Tony Costa's little brother?\n\nA: Tony Costa's little brother is Billy Costa."
 
-          question_1 = "\n\n\nQ: How to choose what business to start?\n\nA: First off don't be in a rush. Look around you, see what problems you or other people are facing, and solve one of these problems if you see some overlap with your passions or skills. Or, even if you don't see an overlap, imagine how you would solve that problem anyway. Start super, super small."
-          question_2 = "\n\n\nQ: Q: Should we start the business on the side first or should we put full effort right from the start?\n\nA:   Always on the side. Things start small and get bigger from there, and I don't know if I would ever “fully” commit to something unless I had some semblance of customer traction. Like with this product I'm working on now!"
-          question_3 = "\n\n\nQ: Should we sell first than build or the other way around?\n\nA: I would recommend building first. Building will teach you a lot, and too many people use “sales” as an excuse to never learn essential skills like building. You can't sell a house you can't build!"
-          question_4 = "\n\n\nQ: Andrew Chen has a book on this so maybe touché, but how should founders think about the cold start problem? Businesses are hard to start, and even harder to sustain but the latter is somewhat defined and structured, whereas the former is the vast unknown. Not sure if it's worthy, but this is something I have personally struggled with\n\nA: Hey, this is about my book, not his! I would solve the problem from a single player perspective first. For example, Gumroad is useful to a creator looking to sell something even if no one is currently using the platform. Usage helps, but it's not necessary."
-          question_5 = "\n\n\nQ: What is one business that you think is ripe for a minimalist Entrepreneur innovation that isn't currently being pursued by your community?\n\nA: I would move to a place outside of a big city and watch how broken, slow, and non-automated most things are. And of course the big categories like housing, transportation, toys, healthcare, supply chain, food, and more, are constantly being upturned. Go to an industry conference and it's all they talk about! Any industry…"
-          question_6 = "\n\n\nQ: How can you tell if your pricing is right? If you are leaving money on the table\n\nA: I would work backwards from the kind of success you want, how many customers you think you can reasonably get to within a few years, and then reverse engineer how much it should be priced to make that work."
-          question_7 = "\n\n\nQ: Why is the name of your book 'the minimalist entrepreneur' \n\nA: I think more people should start businesses, and was hoping that making it feel more “minimal” would make it feel more achievable and lead more people to starting-the hardest step."
-          question_8 = "\n\n\nQ: How long it takes to write TME\n\nA: About 500 hours over the course of a year or two, including book proposal and outline."
-          question_9 = "\n\n\nQ: What is the best way to distribute surveys to test my product idea\n\nA: I use Google Forms and my email list / Twitter account. Works great and is 100% free."
-          question_10 = "\n\n\nQ: How do you know, when to quit\n\nA: When I'm bored, no longer learning, not earning enough, getting physically unhealthy, etc… loads of reasons. I think the default should be to “quit” and work on something new. Few things are worth holding your attention for a long period of time."
-
-          return (header + chosen_sections.join + question_1 + question_2 + question_3 + question_4 + question_5 + question_6 + question_7 + question_8 + question_9 + question_10 + "\n\n\nQ: " + question + "\n\nA: "), (chosen_sections.join)
+          return (header + chosen_sections.join + question_1 + question_2 + question_3 + question_4 + question_5 + question_6 + question_7 + question_8 + question_9 + question_10 + question_11 + "\n\n\nQ: " + question + "\n\nA: "), (chosen_sections.join)
       end
 
       def find_row_by_title(csv_table, title)
