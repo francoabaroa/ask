d8b3f97c31549525d93f2ebdaa0b0112ca64b2ff
Franco Abaroa <franco9395@gmail.com>
2023-04-29 11:34:13 -0400

    Make answer box consistent width

--- frontend/pages/components/AnswerDisplay.tsx
+++ frontend/pages/components/AnswerDisplay.tsx
@@ -6,7 +6,7 @@ const AnswerDisplay: React.FC<AnswerDisplayProps> = ({ answer }) => {
   if (answer === '') return null;
 
   return (
-    <div className="border border-gray-300 p-4 mt-4">
+    <div className="w-full sm:w-90 border border-gray-300 p-4 mt-4">
       <h3 className="font-bold">Answer:</h3>
       <p>{answer}</p>
     </div>
